ff48dc1979f5978f13eba77eed6ac6699c2af8cd
Pietro Montoro Rigetto <pietro.montoro@gmail.com>
2019-05-23 01:46:41 -0300

    making new querys

--- server.py
+++ server.py
@@ -14,7 +14,13 @@ mongo = PyMongo(app)
 
 @app.route('/postgres')
 def insert():
-    cursor.execute("SELECT * FROM conta;")
+    subqueryString = 'SELECT \"Id_Prod\" FROM public.\"Produto\" WHERE \"Id_Prod\" BETWEEN 1 AND 250000 AND (CAST(\"Valor_Unit\" AS TEXT) LIKE \'%1%\' OR CAST(\"Valor_Unit\" AS TEXT) LIKE \'%0%\') AND \"Versao\" > 2004'
+    cursor.execute(subqueryString)
+    print(subqueryString)
+    idResult = cursor.fetchall()
+    queryString = 'SELECT * FROM public.\"Pedido\" WHERE \"Produto_Id_Prod\" IN (\''+ '\',\''.join(idResult)  +'\') AND \"Quantidade\" BETWEEN 10 AND 75 AND \"Valor_Total\" >= 1554 AND \"Conta_Cnpj\" LIKE \'%1%\' ORDER BY \"Valor_Total\" DESC'
+    print(queryString);
+    cursor.execute(queryString)
     resultlist = cursor.fetchall()
     f = open('queryPostgres.txt', 'w+')
     for result in resultlist:
@@ -25,7 +31,9 @@ def insert():
 @app.route('/mongo')
 def show():
     contas = mongo.db.contas
-    test = contas.find_one({'teste': 'teste'})
+    produto = mongo.db.produto
+    #'SELECT \"Id_Prod\" FROM public.\"Produto\" WHERE \"Id_Prod\" BETWEEN 1 AND 250000 AND (CAST(\"Valor_Unit\" AS TEXT) LIKE \'%1%\' OR CAST(\"Valor_Unit\" AS TEXT) LIKE \'%0%\') AND \"Versao\" > 2004'
+    query = [{'$match':{'Quantidade':{'$gte': 1,'$lte': 75}, 'Valor_Total':{'$gte': 1554}, 'Conta_Cnpj': '%o%', 'Id_Prod': produto.find([{'$match': 'Id_Prod': {'$gte': 1,'$lte': 250000}, {'$or': {'Valor_Unit': '%1%', 'Valor_Unit': '%0%'}}, 'Versao': {'$gt': 2004}}])}},{'$sort':{"Valor_Total": 1.0}}, {'$project': {'Id_Ped': 1.0, 'Conta_Cnpj': 1.0, 'Quantidade': 1.0, 'Valor_Total': 1.0,'Produto_Id_Prod': 1.0, 'Forma_Pagamento': 1.0}}]
     f = open('queryMongo.txt', 'w+')
     f.write('melhorou '+ test['teste'] + ' ' + test['teste2'] + ' ' + test['teste3'] + ' '+  test['mostrando'])
     f.close()
